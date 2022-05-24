4e7407d8eece29245079712287eb37fbb1de0430
mao <miymao78.d@gmail.com>
2022-05-25 04:46:03 +0900

    feat: Change to old format.

---
+++ XcodeProject/UaaLExample/UaaLExample/MainViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
